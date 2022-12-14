2d0cf34a21dee6993cad00fa9af08be6b59ce356
nasrul irfan <nasrulirfan173@gmail.com>
2022-12-14 09:48:01 +0700

    menyelesaikan tugas3

--- lib/ui/Produk_detail.dart
+++ lib/ui/Produk_detail.dart
@@ -17,7 +17,7 @@ class _ProdukDetailState extends State<ProdukDetail> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Detail Produk'),
+        title: const Text('Detail Produkk'),
       ),
       body: Column(
         children: [
